22a18ed84fbbb299d5b0e0cd88f485330e92ab99
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-02-16 14:19:53 +0000

    Add trajectories binary

---
+++ src/bin/trajectories.rs
@@ -0,0 +1,128 @@
+use std::io:: Result;
+use std::path::Path;
+
+use clap::Parser;
+use nalgebra::Point3;
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::prelude::*;
+
+use sentinel::configure::Configure;
+use sentinel::geometry::Geometry;
+use sentinel::field::Field;
+use sentinel::tracker::Tracker;
+use sentinel::io::CsvWriter;
+use sentinel::invalid_input;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CLI {
+
+    #[arg(short, long)]
+    conf: String,
+
+    #[arg(short, long)]
+    n_events: usize,
+
+    #[arg(short, long, default_value_t=1000)]
+    batch_size: usize,
+
+    #[arg(short, long)]
+    output: String,
+
+    #[arg(long, default_value_t=false)]
+    overwrite: bool,
+}
+
+pub fn main() -> Result<()> {
+    let args = CLI::parse();
+    let conf = Configure::new(&args.conf).unwrap();
+    let path = Path::new(&args.output);
+    if path.exists() & !args.overwrite {
+        return invalid_input!("Outfile already exists!");
+    }
+
+    rayon::ThreadPoolBuilder::new().num_threads(10).build_global().unwrap();
+
+    let header   = "event x y z t".split(" ")
+                                  .map(|v| v.to_string())
+                                  .collect::<Vec<String>>();
+    let mut writer = CsvWriter::new(&path.to_str().unwrap(), " ", header).unwrap();
+    let geometry   = Geometry::new(conf.rmin, conf.form_factor, conf.zmin, conf.zmax);
+    let field      = Field::from_file(&conf.field_file);
+    let tracker    = Tracker::new(field, geometry, conf.t_step);
+
+    let nbatch = args.n_events.div_ceil(args.batch_size);
+    let pb = ProgressBar::new(nbatch as u64);
+    pb.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.magenta} [{elapsed_precise}] \
+             [{bar:40.bold.#c000ff/#5a00aa}] \
+             {pos:>7}/{len:7} \
+             {percent:>3}% \
+             ETA {eta_precise}"
+        ).unwrap()
+         .progress_chars("█▉▊▋▌▍▎▏ ") // other: "█▓░" "█▇▆▅▄▃▂▁ "
+    );
+    for batch in 0..nbatch {
+        let data : Vec<Vec<Vec<f64>>> =
+        (0..args.batch_size).into_par_iter()
+                            .map( |evt| {
+                                let evt = evt + batch * args.batch_size;
+                                let (x0, y0, z0) = (0., 0., -9.);
+                                tracker.propagate_from(Point3::new(x0, y0, z0))
+                                       .into_iter()
+                                       .enumerate()
+                                       .map(|(i, p)| vec![evt as f64, p.x, p.y, p.z, i as f64 * conf.t_step])
+                                       .collect::<Vec<Vec<f64>>>()
+                            })
+                            .collect();
+        data.into_iter().for_each(|evt| {
+            for row in evt {
+                writer.write(row).unwrap();
+            }
+        });
+        pb.inc(1);
+    }
+    pb.finish();
+
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use nalgebra::{Point2, Point3, Vector2};
+    use sentinel::field_point::FieldPoint;
+    use sentinel::field::Field;
+    use sentinel::tracker::Tracker;
+    use sentinel::geometry::Geometry;
+
+
+    fn homogeneous_field() -> Field {
+        let points   = vec![
+            FieldPoint::new(Point2::<f64>::new(0., 11.), 1., 0, Vector2::new(0., -1.)), // startpoint
+            FieldPoint::new(Point2::<f64>::new(0.,  1.), 1., 0, Vector2::new(0., -1.)), //
+            FieldPoint::new(Point2::<f64>::new(0.,  0.), 1., 0, Vector2::zeros()     ), //   endpoint
+        ];
+        Field::new(points)
+    }
+
+    #[test]
+    fn test_it_runs() {
+        let geometry = Geometry::new(1., 1., 0., 10.);
+        let tracker  = Tracker::new(homogeneous_field(), geometry, 1e-2);
+        let t = tracker.propagate_from(Point3::new(0., 0., -9.));
+        assert!(t.len() > 100, "Track length too short: {}", t.len());
+        assert!(t.last().unwrap().z > -2e-2); // close to zmin
+    }
+
+    #[test]
+    fn test_it_runs_slow() {
+        let geometry = Geometry::new(1., 1., 0., 10.);
+        let tracker  = Tracker::new(homogeneous_field(), geometry, 5e-4);
+        let t = tracker.propagate_from(Point3::new(0., 0., -9.));
+
+        assert!(t.len() > 1000, "Track length too short: {}", t.len());
+        assert!(t.last().unwrap().z > -1e-3); // close to zmin
+    }
+}
