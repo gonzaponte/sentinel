dda20bb3eee70d6cdd86727fd99ef2465052671d
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-02-23 17:08:01 +0000

    Add Writer trait

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -7,3 +7,15 @@ pub use csv::CsvWriter;
 
 pub use hdf5::read_hdf5;
 pub use hdf5::Hdf5Writer;
+
+use std::io;
+pub trait Writer<T> {
+    fn write(&self, value: T) -> io::Result<()>;
+
+    fn write_many(&self, values: Vec<T>) -> io::Result<()> {
+        for v in values.into_iter() {
+            self.write(v)?;
+        }
+        Ok(())
+    }
+}
