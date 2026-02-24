baa48528df49ccd9be50744a99b9a4a8e71eab19
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-02-24 09:13:31 +0000

    Add writer selection to endpoints binary

--- src/bin/endpoints.rs
+++ src/bin/endpoints.rs
@@ -1,15 +1,18 @@
 use std::io:: Result;
 use std::path::Path;
+use std::rc::Rc;
 
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
+use hdf5_metno as hdf5;
 
 use sentinel::configure::Configure;
 use sentinel::geometry::Cone;
 use sentinel::field::Field;
 use sentinel::tracker::Tracker;
-use sentinel::io::CsvWriter;
+use sentinel::io::{Writer, CsvWriter, Hdf5Writer};
+use sentinel::io::hdf5_types::Endpoint;
 use sentinel::invalid_input;
 
 #[derive(Parser, Debug)]
@@ -33,6 +36,9 @@ struct CLI {
 
     #[arg(short, long, default_value_t=10)]
     threads: usize,
+
+    #[arg(long, default_value_t=false)]
+    csv: bool,
 }
 
 pub fn main() -> Result<()> {
@@ -45,14 +51,23 @@ pub fn main() -> Result<()> {
 
     rayon::ThreadPoolBuilder::new().num_threads(args.threads).build_global().unwrap();
 
-    let header   = "x0 y0 z0 x1 y1 z1 t".split(" ")
-                                        .map(|v| v.to_string())
-                                        .collect::<Vec<String>>();
-    let mut writer = CsvWriter::new(&path.to_str().unwrap(), " ", header).unwrap();
     let geometry   = Cone::new(conf.rmin, conf.form_factor, conf.zmax);
     let field      = Field::from_file(&conf.field_file, conf.field_to_mm, conf.field_to_Vpercm, true);
     let tracker    = Tracker::new(field, geometry.clone(), conf.t_step);
     let sampler    = conf.generator.sampler(&geometry);
+
+    let writer : Box<dyn Writer<Endpoint>> =
+        if args.csv {
+            let header = "event x0 y0 z0 x1 y1 z1 t".split(" ")
+                                                    .map(|v| v.to_string())
+                                                    .collect::<Vec<String>>();
+
+            Box::new(CsvWriter::new(&path.to_str().unwrap(), " ", header).unwrap())
+        }
+        else {
+            let file = hdf5::File::create(&path.to_str().unwrap()).unwrap();
+            Box::new(Hdf5Writer::<Endpoint>::new(Rc::new(file), "endpoints", 1024).unwrap())
+        };
 
     let nbatch = args.n_events.div_ceil(args.batch_size);
     let pb = ProgressBar::new(nbatch as u64);
@@ -67,24 +82,26 @@ pub fn main() -> Result<()> {
          .progress_chars("█▉▊▋▌▍▎▏ ") // other: "█▓░" "█▇▆▅▄▃▂▁ "
     );
     pb.reset(); // force drawing
-    for _batch in 0..nbatch {
-        let data : Vec<Vec<f64>> =
+    for batch in 0..nbatch {
+        let data : Vec<Endpoint> =
         (0..args.batch_size).into_par_iter()
-                            .map( |_| {
+                            .map( |evt| {
+                                let evt          = evt + batch * args.batch_size;
                                 let starting_pos = sampler.sample(&geometry);
                                 let trajectory = tracker.propagate_from(starting_pos.clone());
                                 let last = trajectory.last().unwrap();
-                                let (x1, y1, z1) = (last.x, last.y, last.z);
-                                let t = trajectory.len() as f64 * conf.t_step;
-                                vec![ starting_pos.x , starting_pos.y , starting_pos.z
-                                    ,              x1,              y1,              z1
-                                    , t
-                                    ]
+                                Endpoint{ event: evt as u32
+                                        , x0   : starting_pos.x as f32
+                                        , y0   : starting_pos.y as f32
+                                        , z0   : starting_pos.z as f32
+                                        , x1   :         last.x as f32
+                                        , y1   :         last.y as f32
+                                        , z1   :         last.z as f32
+                                        , t    : trajectory.len() as f32 * conf.t_step as f32
+                                }
                             })
                             .collect();
-        data.into_iter().for_each(|x| {
-            writer.write(x).unwrap();
-        });
+        writer.write_many(data)?;
         pb.inc(1);
     }
     pb.finish();
