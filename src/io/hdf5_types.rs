14626c132449cb286a035ba2a0730e921a575d7b
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-03-31 09:03:53 +0000

    Add LT hdf5 types

--- src/io/hdf5_types.rs
+++ src/io/hdf5_types.rs
@@ -33,3 +33,38 @@ pub struct TrajectoryPoint {
     pub z    : f32,
     pub t    : f32,
 }
+
+#[derive(hdf5::H5Type, Clone, PartialEq, Debug)]
+#[repr(C)]
+pub struct S1LightTablePoint {
+    pub sensor_id: u16,
+    pub z        : f32,
+    pub x        : f32,
+    pub y        : f32,
+    pub nevt     : u32,
+    pub prob     : f32,
+}
+
+
+#[derive(hdf5::H5Type, Clone, PartialEq, Debug)]
+#[repr(C)]
+pub struct S2LightTablePoint {
+    pub sensor_id: u16,
+    pub x        : f32,
+    pub y        : f32,
+    pub nevt     : u32,
+    pub prob     : f32,
+}
+
+#[derive(hdf5::H5Type, Clone, PartialEq, Debug)]
+#[repr(C)]
+pub struct DriftMapPoint {
+    pub r0   : f32,
+    pub z0   : f32,
+    pub x1   : f32,
+    pub y1   : f32,
+    pub n_dst: u32,
+    pub n_src: u32,
+    pub t_ave: f32,
+    pub ok   : bool,
+}
